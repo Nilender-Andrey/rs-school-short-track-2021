a6ddb4c5288705f94ccbaa267f134386a4bba5ce
Nilender-Andrey <nilenderav@mail.ru>
2021-04-09 00:31:38 +0400

    feat: add solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let n = 1;
+  let strNew = '';
+  for (let i = 0; i < str.length; i++) {
+    while (str[i] === str[i + 1]) {
+      n++; i++;
+    }
+    if (n === 1) strNew += str[i];
+    else {
+      strNew = strNew + n + str[i];
+      n = 1;
+    }
+  }
+  return strNew;
 }
 
 module.exports = encodeLine;
